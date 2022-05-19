2cbe54be210c5b1b0d4a8bd0225930d1d4375531
LeppoXena <francesco.lepore@xenagames.com>
2022-05-19 20:19:19 +0200

    Minor adjustement

--- data.js
+++ data.js
@@ -4,9 +4,9 @@
 
 // Milestone 3 Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l’utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
 
-// BONUS 1- modificare la struttura dati fornita e valorizzare la proprietà “color” in modo dinamico: generare in modo casuale un codice colore, sapendo che la notazione esadecimale è formata dal simbolo “#” seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
+// BONUS 1 - modificare la struttura dati fornita e valorizzare la proprietà “color” in modo dinamico: generare in modo casuale un codice colore, sapendo che la notazione esadecimale è formata dal simbolo “#” seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
 
-// BONUS 2- popolare le options della select della milestone 3 dinamicamente.
+// BONUS 2 - popolare le options della select della milestone 3 dinamicamente.
 
 const allIcon = [
 	{
